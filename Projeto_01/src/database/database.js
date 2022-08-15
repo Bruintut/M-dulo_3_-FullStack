4b7a9ea1df44f4ccabd21e031fd0d2e0802ac71b
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-08-15 13:58:13 -0300

    feat: inclued mongoDb

---
+++ Projeto_01/src/database/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = () => {
+    mongoose.connect('mongodb://localhost:27017/knights-db', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log("MongoDb CONNECT!")).catch((error) => 
+    console.log(`Erro ao conectar com o MongoDb, erro: ${error}`),
+    );
+};
+
+module.exports = connectToDatabase;
