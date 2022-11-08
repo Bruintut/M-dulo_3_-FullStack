4d1fd31b8c92678fa465c9487d96637a9e171896
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-07 22:42:12 -0300

    convencional commit

--- Projeto_01/src/database/database.js
+++ Projeto_01/src/database/database.js
@@ -1,7 +1,9 @@
+
 const mongoose = require('mongoose');
 
+
 const connectToDatabase = () => {
-    mongoose.connect('mongodb://localhost:27017/knights-db', {
+    mongoose.connect(process.env.URI_DATABASE, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => console.log("MongoDb CONNECT!")).catch((error) => 
