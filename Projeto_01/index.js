934e13b7d356206646ba08268be2d2219fcc5e61
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-08 14:16:33 -0300

    conventional commit

--- Projeto_01/index.js
+++ Projeto_01/index.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 const routes = require('./src/routes/knight.route');
 const connectToDatabase = require('./src/database/database');
 
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 3000;
 const app = express();
 
 connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
-app.use('/knights',routes);
+app.use('/knights', routes);
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
