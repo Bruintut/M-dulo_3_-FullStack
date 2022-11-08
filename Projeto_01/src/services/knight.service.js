4a2189c6c3987bb3b05b2e8f3eafdcb22e170606
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-08 15:20:21 -0300

    use prettier

--- Projeto_01/src/services/knight.service.js
+++ Projeto_01/src/services/knight.service.js
@@ -1,40 +1,35 @@
+const Knight = require('../models/Knight');
 
+const findAllKnightsService = async () => {
+  const knights = await Knight.find();
 
-  const Knight = require('../models/Knight')
+  return knights;
+};
 
+const findByIdKnightService = async (id) => {
+  const knight = await Knight.findById(id);
+  return knight;
+};
 
-  const findAllKnightsService = async () =>{
-    const knights = await Knight.find();
+const createKnightService = async (newKnight) => {
+  const newId = await Knight.create(newKnight);
+  newKnight.id = newId;
+  return newKnight;
+};
 
-    return knights;
-  };
+const updateKnightService = async (id, knightEdited) => {
+  const updatedKnight = await Knight.findByIdAndUpdate(id, knightEdited);
+  return updatedKnight;
+};
 
-  const findByIdKnightService = async (id) => {
-    const knight = await Knight.findById(id);
-    return knight;
-     
-  };
+const deleteKnightService = async (id) => {
+  return Knight.findByIdAndDelete(id);
+};
 
-  const createKnightService = async (newKnight) =>{
-    const newId = await Knight.create(newKnight);
-    newKnight.id = newId;
-    return newKnight;
-  };
-
-  const updateKnightService = async (id, knightEdited) => {
-    const updatedKnight = await Knight.findByIdAndUpdate(id, knightEdited);
-    return updatedKnight;
-  };
-
-  const deleteKnightService = async (id) => {
-    return Knight.findByIdAndDelete(id);
-  }
-
-  module.exports = {
-    findAllKnightsService,
-    findByIdKnightService,
-    createKnightService,
-    updateKnightService,
-    deleteKnightService
-
-  };
+module.exports = {
+  findAllKnightsService,
+  findByIdKnightService,
+  createKnightService,
+  updateKnightService,
+  deleteKnightService,
+};
