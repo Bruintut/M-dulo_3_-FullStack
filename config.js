10b20d3598acc7ecc7e85d46f25e11aaabe24f8d
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-09 23:43:03 -0300

    prettier

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 module.exports = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {key: 'Acess-Control-Allow-Credentials', value:'true'},
-                    {key: 'Acess-Control-Allow-Origin', value:'*'},
-                    {key: 'Acess-Control-Allow-Methods', value:'GET,OPTION,PATCH,DELETE,POST,PUT'},
-                    {key: 'Acess-Control-Allow-Credentials', value:'X-CSRF-Token, X-Requeested-With,'},
-                ]
-            }
-        ]
-    }
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Acess-Control-Allow-Credentials', value: 'true' },
+          { key: 'Acess-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Acess-Control-Allow-Methods',
+            value: 'GET,OPTION,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Acess-Control-Allow-Credentials',
+            value: 'X-CSRF-Token, X-Requeested-With,',
+          },
+        ],
+      },
+    ];
+  },
+};
